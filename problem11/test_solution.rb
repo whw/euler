281d9c7d20fcccba2eadc21e47c47bb4cc95e182
William Williams <whw@tsumobi.com>
2012-07-11 16:39:04 -0700

    Solved problem 11

---
+++ problem11/test_solution.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+
+class TestSolution < Test::Unit::TestCase
+
+  INPUT =
+  OUTPUT =
+
+  SOLUTION = File.join(File.dirname(__FILE__), "solution")
+
+  def test_solution
+    results = `#{SOLUTION} #{INPUT}`.chomp
+    raise "error!" unless $?.success?
+
+    assert_equal(results.to_i, OUTPUT)
+  end
+
+end
